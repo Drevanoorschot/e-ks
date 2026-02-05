5d698c992071a298978a1f5afe52a0653e7c70cd
Marlon Baeten <marlon@tweedegolf.com>
2026-02-05 19:17:19 +0100

    Overlay / steps styling

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -4,4 +4,4 @@
 pub const DEFAULT_DATE_FORMAT: &str = "%d-%m-%Y";
 
 /// Default datetime format
-pub const DEFAULT_DATE_TIME_FORMAT: &str = "%d-%m-%Y %H:%M:%S";
+pub const DEFAULT_DATE_TIME_FORMAT: &str = "%d-%m-%Y %H:%M";
